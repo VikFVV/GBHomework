a38f74ce530d5d6c9116617e318ff94f084d2af2
Viktor <zunayka@gmail.com>
2022-12-23 15:59:09 +1200

    Task08 complete

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
