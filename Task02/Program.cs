06ec3736088fe779ed20b8dfdc73016e028a6f6f
Viktor <zunayka@gmail.com>
2022-12-23 15:49:57 +1200

    Task02 complete

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine());
+Write("Введите число b: ");
+int b = int.Parse(ReadLine());         
+if (a > b)
+{
+                  Write(a);
+}
+                    else if (b > a)
+
+                    {
+                    Write(b);
+                    }
